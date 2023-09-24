de41821d7f35226195966422cbfe0172ccc8bd88
dingPie <cpie1216@gmail.com>
2023-09-24 18:30:35 +0900

    로그인 임시 로직 구현 중

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Box, Button, Flex, Pressable, Text, VStack } from 'native-base';
 
+import auth from '@react-native-firebase/auth';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 import { useNavigation } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
 
@@ -10,8 +15,53 @@ import { AuthStackParamList } from '@/navigations/type';
 
 type LoginNavigationProps = StackNavigationProp<AuthStackParamList, 'Login'>;
 
+GoogleSignin.configure({
+  webClientId:
+    '507352263432-flnmeesbeuvcigs02jpicnniaaun01ge.apps.googleusercontent.com',
+});
+
 const LoginScreen = () => {
   const navigation = useNavigation<LoginNavigationProps>();
+
+  const onPressGoogleLoginButton = async () => {
+    try {
+      await GoogleSignin.hasPlayServices({
+        showPlayServicesUpdateDialog: true,
+      });
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      const result = await auth().signInWithCredential(googleCredential);
+
+      const { uid, email } = result.user;
+      const providerId = result.additionalUserInfo?.providerId;
+
+      // P_TODO: 이제 여기서 로그인 API 쳐야 함.
+      // P_TODO: prod에서는 어떻게 되는지 확인해야 함.
+
+      console.log({
+        uid,
+        email,
+        providerId,
+      });
+
+      return {
+        uid,
+        email,
+        providerId,
+      };
+    } catch (err: any) {
+      if (err.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.error('유저가 취소함.'); // 유저가 취소
+      } else if (err.code === statusCodes.IN_PROGRESS) {
+        console.error('절차 진행중');
+      } else if (err.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.error('구글플레이 서비스 이용 불가?');
+        // play services not available or outdated
+      } else {
+        console.error('그 외에 에러', err);
+      }
+    }
+  };
 
   return (
     <VStack
@@ -33,6 +83,7 @@ const LoginScreen = () => {
 
       <VStack mb="20%" w="100%" space="8px">
         <Pressable
+          onPress={async () => await onPressGoogleLoginButton()}
           flexDir="row"
           alignItems="center"
           bgColor="white"
