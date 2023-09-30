d8e23cfa32cb8e885c759ea118c34ae083d270b4
dingPie <cpie1216@gmail.com>
2023-09-30 17:22:34 +0900

    feat(CreateRefrigeratorScreen): 🎸화면 Ui 퍼블리싱
    기능 구현해야 함 / 느린거 고쳐야 함

--- src/constants/label.ts
+++ src/constants/label.ts
@@ -7,4 +7,16 @@ export const LABEL = {
       waiting: '승인 대기중',
     },
   },
+  REFRIGERATOR_SPACE: {
+    SHAPE_TYPE: {
+      shelf: '일반형',
+      drawer: '서랍형',
+      door: '문형',
+    },
+    PURPOSE_TYPE: {
+      refrigeration: '냉장실',
+      freeze: '냉동실',
+      kimchi: '김치냉장고',
+    },
+  },
 };
--- src/screens/CreateRefrigeratorScreen/CreateRefrigeratorScreen.tsx
+++ src/screens/CreateRefrigeratorScreen/CreateRefrigeratorScreen.tsx
@@ -1,31 +1,109 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-import { Box, Button, Input, Text } from 'native-base';
+import { Box, Button, FlatList, Flex, Input, Text, VStack } from 'native-base';
+import { FormProvider, useWatch } from 'react-hook-form';
 
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { MainStackParamList } from '@/navigations/type';
 
+import BaseSettingWrapper from './components/BaseSettingWrapper';
+import RefrigeratorSpaceInputItem from './components/RefrigeratorSpaceInputItem';
+import useCreateRefrigeratorForm, {
+  emptyRefrigeratorSpaceItem,
+} from './useCreateRefrigeratorForm';
+
 type MainNavigationProp = StackNavigationProp<MainStackParamList>;
 
 const CreateRefrigeratorScreen = () => {
   const navigation = useNavigation<MainNavigationProp>();
 
-  return (
-    <Box
-      flex={1}
-      h="100%"
-      borderRadius="10px"
-      borderColor="white"
-      color="primary.400"
-    >
-      <Text size="xl">냉장고 생성 페이지</Text>
+  const createRefrigeratorMethod = useCreateRefrigeratorForm();
 
-      <Button onPress={() => navigation.navigate('BottomTab')}>
-        <Text>바텀탭으로 가기</Text>
-      </Button>
-    </Box>
+  const refrigeratorSpaceList = useWatch({
+    control: createRefrigeratorMethod.control,
+    name: 'refrigeratorSpaceList',
+  });
+
+  const onPressAddEmptyRefrigeratorButton = useCallback(() => {
+    const newList = [...refrigeratorSpaceList, emptyRefrigeratorSpaceItem];
+    createRefrigeratorMethod.setValue('refrigeratorSpaceList', newList, {
+      shouldValidate: true,
+    });
+  }, [createRefrigeratorMethod, refrigeratorSpaceList]);
+
+  const onPressRemoveEmptyRefrigeratorButton = useCallback(
+    (index: number) => {
+      const removedList = refrigeratorSpaceList.filter(
+        (_, idx) => index !== idx,
+      );
+      createRefrigeratorMethod.setValue('refrigeratorSpaceList', removedList, {
+        shouldValidate: true,
+      });
+    },
+    [createRefrigeratorMethod, refrigeratorSpaceList],
+  );
+
+  // 냉장고 생성 버튼
+  const onPressCreateRefrigeratorButton = useCallback(() => {
+    console.log('냉장고 생성');
+  }, []);
+
+  return (
+    <FormProvider {...createRefrigeratorMethod}>
+      <VStack flex={1} h="100%" bgColor="white">
+        <FlatList
+          data={refrigeratorSpaceList}
+          renderItem={({ item, index }) => {
+            return (
+              <RefrigeratorSpaceInputItem
+                key={index}
+                index={index}
+                onPressRemoveEmptyRefrigeratorButton={
+                  onPressRemoveEmptyRefrigeratorButton
+                }
+              />
+            );
+          }}
+          keyExtractor={(_, index) => index.toString()}
+          ListHeaderComponent={() => (
+            <>
+              <BaseSettingWrapper />
+              <Text size="2xl.bold" mb="16px">
+                기본 설정
+              </Text>
+            </>
+          )}
+          ListFooterComponent={() => {
+            return (
+              <Button onPress={onPressAddEmptyRefrigeratorButton} py="4px">
+                <Text color="white" size="4xl.bold">
+                  +
+                </Text>
+              </Button>
+            );
+          }}
+          flex={1}
+          py="24px"
+          bgColor="white"
+          ListFooterComponentStyle={{
+            marginBottom: 40,
+          }}
+          contentContainerStyle={{
+            paddingHorizontal: 16,
+          }}
+        />
+
+        <Flex p="16px">
+          <Button rounded="full" onPress={onPressCreateRefrigeratorButton}>
+            <Text color="white" size="lg.bold">
+              냉장고 생성
+            </Text>
+          </Button>
+        </Flex>
+      </VStack>
+    </FormProvider>
   );
 };
 
