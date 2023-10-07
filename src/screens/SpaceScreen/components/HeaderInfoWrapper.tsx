d31e88e437156cc1d044aca7b15d1f707af29ada
dingPie <cpie1216@gmail.com>
2023-10-07 21:14:00 +0900

    feat(HeaderInfoWrapper.tsx): 🎸냉장고 칸 별 상단 정보 및 filter 구현

---
+++ src/screens/SpaceScreen/components/HeaderInfoWrapper.tsx
@@ -0,0 +1,90 @@
+import React, { Dispatch, SetStateAction, memo } from 'react';
+
+import { Checkbox, HStack, Text, VStack } from 'native-base';
+
+import { RefrigeratorSpaceItemType } from '@/apis/refrigerator-space/types/model/list-model';
+
+import RefrigeratorPurposeIcon from '@/components/#Atoms/RefrigeratorPurposeIcon';
+import RefrigeratorShapeIcon from '@/components/#Atoms/RefrigeratorShapeIcon';
+
+import { LABEL } from '@/constants/label';
+
+interface HeaderInfoWrapperProps {
+  refrigeratorSpaceInfo: RefrigeratorSpaceItemType;
+  totalCount?: number;
+  isMine: boolean;
+  setIsMine: Dispatch<SetStateAction<boolean>>;
+}
+
+const HeaderInfoWrapper = ({
+  refrigeratorSpaceInfo,
+  totalCount = 0,
+  isMine,
+  setIsMine,
+}: HeaderInfoWrapperProps) => {
+  return (
+    <VStack
+      space="8px"
+      bgColor="gray.300"
+      borderRadius="8px"
+      p="16px"
+      mb="12px"
+    >
+      <HStack justifyContent="space-between" alignItems="center">
+        <Text size="xl.bold"> {refrigeratorSpaceInfo.name} </Text>
+      </HStack>
+      <HStack space="16px" alignItems="center" justifyContent="flex-end">
+        <HStack space="8px">
+          <RefrigeratorPurposeIcon
+            purposeType={refrigeratorSpaceInfo.purposeType}
+            size={24}
+            color="black"
+          />
+          <Text size="md.bold">
+            {
+              LABEL.REFRIGERATOR_SPACE.PURPOSE_TYPE[
+                refrigeratorSpaceInfo.purposeType
+              ]
+            }
+          </Text>
+        </HStack>
+        <HStack space="8px">
+          <RefrigeratorShapeIcon
+            shapeType={refrigeratorSpaceInfo.shapeType}
+            size={24}
+            color="black"
+          />
+          <Text size="md.bold">
+            {
+              LABEL.REFRIGERATOR_SPACE.SHAPE_TYPE[
+                refrigeratorSpaceInfo.shapeType
+              ]
+            }
+          </Text>
+        </HStack>
+      </HStack>
+
+      <HStack justifyContent="space-between">
+        <Text size="md.bold">현재 보관된 상품</Text>
+        <Text size="md.bold">
+          {`${totalCount} / ${refrigeratorSpaceInfo.maxCountPerSpace} 개`}
+        </Text>
+      </HStack>
+
+      <HStack justifyContent="space-between">
+        <Text size="md.bold">내 상품만 표시</Text>
+        <Checkbox
+          value=""
+          isChecked={isMine}
+          onChange={setIsMine}
+          size="md"
+          mr="-14px"
+        >
+          {''}
+        </Checkbox>
+      </HStack>
+    </VStack>
+  );
+};
+
+export default memo(HeaderInfoWrapper);
